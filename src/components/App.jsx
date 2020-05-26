66bf7f49c893e87035d7b80790e3016f625661ae
SamM922 <samuel.maturo@gmail.com>
2020-05-25 18:32:28 -0700

    Added attributes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,8 +8,28 @@ class App extends Component {
     super(props);
     this.state = {
         userScore: 0,
-        compScore: 0
+        compScore: 0,
+        userSkillShot: 0,
+        userSkillGK: 0,
+        userSkillDribble: 0,
+        userSkillPass: 0,
+        userSkillInt: 0,
+        userSkillTackle: 0
+        // compSkillShot: 0,
+        // compSkillGK: 0,
+        // compSkillDribble: 0,
+        // compSkillPass: 0,
+        // compSkillInt: 0,
+        // compSkillTackle: 0
     };
+  }
+  start = () => {
+    this.setState({userSkillShot: Math.random * 50 + 50})
+    this.setState({userSkillGK: Math.random * 50})
+    this.setState({userSkillPass: Math.random * 50 + 50})
+    this.setState({userSkillDribble: Math.random * 50 + 50})
+    this.setState({userSkillInt: Math.random * 50})
+    this.setState({userSkillTackle: Math.random * 50})
   }
   onUserGoal = () => {
     this.setState({userScore: this.state.userScore + 1})
